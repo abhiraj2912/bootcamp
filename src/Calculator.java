6a496c5579ea9825bf94ae9a85de902abc2aaeab
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-21 11:36:59 +0530

    division

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,14 +9,22 @@ public class Calculator {
         return (a-b);
     }
     static double multiplication(double a, double b){
-        return (a+b);
+        return (a*b);
     }
+
+    static double division(double a, double b){
+        return (a/b);
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("Select a operation");
         System.out.println("1. Addition");
         System.out.println("2. Subtraction");
-        System.out.println("2. Multiplication");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
 
 
         Scanner sc = new Scanner(System.in);
@@ -33,6 +41,12 @@ public class Calculator {
                 System.out.println(op1+" - "+op2+" = "+subtraction(op1,op2));
             case 3:
                 System.out.println(op1+" * "+op2+" = "+multiplication(op1,op2));
+            case 4:
+                System.out.println(op1+" / "+op2+" = "+division(op1,op2));
+            default:
+                System.out.println("Invalid Choice");
+
+
 
         }
     }
