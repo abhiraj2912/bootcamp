e21a54ef92eba140847e2066fe8718861fd04a13
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-21 12:02:22 +0530

    add exit

--- src/Calculator.java
+++ src/Calculator.java
@@ -19,32 +19,45 @@ public class Calculator {
 
 
     public static void main(String[] args) {
+        int x=0;
+        while(x==0){
         System.out.println("Select a operation");
         System.out.println("1. Addition");
         System.out.println("2. Subtraction");
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
+        System.out.println("5. Exit");
 
 
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        System.out.println("Enter the first operand");
-        int op1= sc.nextInt();
-        System.out.println("Enter the Second operand");
-        int op2 = sc.nextInt();
-        switch (n){
+            System.out.println("Enter the first operand");
+            int op1 = sc.nextInt();
+            System.out.println("Enter the Second operand");
+            int op2 = sc.nextInt();
+
+        switch (n) {
             case 1:
-                System.out.println(op1+" + "+op2+" = "+addition(op1,op2));
+                System.out.println(op1 + " + " + op2 + " = " + addition(op1, op2));
+                break;
             case 2:
-                System.out.println(op1+" - "+op2+" = "+subtraction(op1,op2));
+                System.out.println(op1 + " - " + op2 + " = " + subtraction(op1, op2));
+                break;
             case 3:
-                System.out.println(op1+" * "+op2+" = "+multiplication(op1,op2));
+                System.out.println(op1 + " * " + op2 + " = " + multiplication(op1, op2));
+                break;
             case 4:
-                System.out.println(op1+" / "+op2+" = "+division(op1,op2));
+                System.out.println(op1 + " / " + op2 + " = " + division(op1, op2));
+                break;
+            case 5:
+                System.out.println("Thanks for using");
+                x = 1;
+                break;
             default:
                 System.out.println("Invalid Choice");
+        }
 
 
 
